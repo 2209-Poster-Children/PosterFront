8250dbefba0e9b3720329dca0b70ac01a3ec3b26
Madi-Webb <madilynwebb98@gmail.com>
2022-11-16 21:03:06 -0700

    built out basic profile compononents

--- src/components/Profile/Login.js
+++ src/components/Profile/Login.js
@@ -1,9 +1,53 @@
-// import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import('./profile.css');
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+
+
+  async function loginFormSubmitHandler(event) {
+    event.preventDefault();
+
+    // TODO: try to log user
+    console.log("logging in...");
+  }
+
+
+  function togglePasswordVisibility(event) {
+    setPasswordVisibility(event.target.checked);
+    let passwordType = document.getElementById("passwordInput");
+    passwordType.type === "password" ?  passwordType.type = "text" : passwordType.type = "password";
+  }
+
+
   return (
-    <div>
-      Log me in, man.
+    <div className='vert-flex-container'>
+      <form  onSubmit={loginFormSubmitHandler}>
+        <label>Enter Username:</label>
+        <input type='text' value={username} onChange={(event) => setUsername(event.target.value)}></input>
+
+        <br />
+
+        <label>Enter Password:</label>
+        <input type='password' value={password} id='passwordInput' onChange={(event) => setPassword(event.target.value)}></input>
+
+        <div className='centered'>
+          <label className='small-text'>Show Password?</label>
+          <input type='checkbox' value={passwordVisibility} onChange={togglePasswordVisibility}></input>
+        </div>
+
+        <br />
+
+        <button type='submit'>Login</button>
+      </form>
+
+      <Link to="/register">Don't have an account? Click here to sign up</Link>
+
     </div>
   )
 }
--- src/components/Profile/Register.js
+++ src/components/Profile/Register.js
@@ -1,9 +1,49 @@
-// import React from 'react';
+import React, { useState } from 'react';
+
+import('./profile.css');
 
 const Register = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+
+
+  async function registerFormSubmitHandler(event) {
+    event.preventDefault();
+
+    // TODO: try to register user
+    console.log("registering...");
+  }
+
+
+  function togglePasswordVisibility(event) {
+    setPasswordVisibility(event.target.checked);
+    let passwordType = document.getElementById("passwordInput");
+    passwordType.type === "password" ?  passwordType.type = "text" : passwordType.type = "password";
+  }
+
+
   return (
-    <div>
-      Make me a log-in
+    <div className='vert-flex-container'>
+      <form  onSubmit={registerFormSubmitHandler}>
+        <label>Enter New Username:</label>
+        <input type='text' value={username} onChange={(event) => setUsername(event.target.value)}></input>
+
+        <br />
+
+        <label>Enter New Password:</label>
+        <input type='password' value={password} id='passwordInput' onChange={(event) => setPassword(event.target.value)}></input>
+
+        <div className='centered'>
+          <label className='small-text'>Show Password?</label>
+          <input type='checkbox' value={passwordVisibility} onChange={togglePasswordVisibility}></input>
+        </div>
+
+        <br />
+
+        <button type='submit'>Register</button>
+      </form>
     </div>
   )
 }
