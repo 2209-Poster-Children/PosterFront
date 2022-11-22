187831e22da78adac0c07ee0e93844d35cde4f82
Andrew Cook <andrewjcook93@gmail.com>
2022-11-22 16:17:55 -0600

    temp styles on shop page

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,8 +4,229 @@ import Searchbar from './Searchbar';
 const Products = () => {
   return (
     <div>
+
       <Searchbar />
-      Stuff for Sale goes in here
+      
+      {/* dummy products */}
+      <div className="products-container">
+        
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+        
+        <div className="product">
+          <img src="http://cdn.akc.org/content/hero/puppy_pictures_header.jpg" height ="175"/>
+          <div className="product-info">
+            <p>Name</p>
+            <p>Rating</p>
+            <p>Price</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/components/Shop/Searchbar.js
+++ src/components/Shop/Searchbar.js
@@ -1,9 +1,23 @@
 import './shop.css'
+import { ImSearch } from 'react-icons/im'
 
 const Searchbar = () => {
   return (
     <div>
-      (SEARCH = Shop/Searchbar.js)
+      <form>
+        
+        <select>
+          <option value="all">All</option>
+          <option value="Movies">Movies</option>  
+          <option value="music">Music</option>
+          <option value="videogames">Video Games</option>
+        </select> 
+
+        <input type="text"></input>
+
+        <button type="submit"><ImSearch /></button>
+
+      </form>
     </div>
   )
 }
