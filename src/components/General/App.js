62f129e5b57a99da3db5c718946b26e37371f4cd
Andrew Cook <andrewjcook93@gmail.com>
2022-11-22 13:23:17 -0600

    added Outlet context

--- src/components/General/App.js
+++ src/components/General/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
 
     return (
         <div>
             <h1>Single Page Web App</h1>
+            
+            <Outlet context />
         </div>
     )
 }
