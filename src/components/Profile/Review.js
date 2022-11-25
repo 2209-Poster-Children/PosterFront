49146d4d78ed02c65339c0051f135b06f2128ddc
Madi-Webb <madilynwebb98@gmail.com>
2022-11-25 14:34:15 -0700

    fixed className error

--- src/components/Profile/Review.js
+++ src/components/Profile/Review.js
@@ -4,51 +4,45 @@ import('./yeisi.css');
 import { BsFillPlusSquareFill } from 'react-icons/ai';
 
 const Review= ({review,idx})=>{
-    console.log(review);
-    console.log ("idx",idx);
+    // console.log(review);
 
     const { userObj: {userData } } = useOutletContext();
-return (
-    <div>
 
-        <div class='review-container'>
-        <button class="add-review">＋</button>
 
-            {
-                review.primaryReview
-            }
-             <div class ='review-element'>
-                <div class = 'review-label'>User</div>
-                <div class = 'review-value'>{userData.user.username}</div>
-            </div>
+    return (
+        <div>
 
-            
-            <div class="image-container">
-                
-            <div img src="https://phantom-marca.unidadeditorial.es/9539821a6a594eca33bde1994c03ecd6/resize/1320/f/jpg/assets/multimedia/imagenes/2021/09/05/16308252543285.jpg"class="image"height='200'></div>
-        
-            </div>
+            <div className='review-container'>
+                <button className="add-review">＋</button>
 
-            <div class = 'review-element'>
-                <div class='review-title'></div>
-                <div class ='review-value'>{review.title}</div>
-            </div>
+                <div className='review-element'>
+                    <div className='review-label'>User</div>
+                    <div className='review-value'>{userData.user.username}</div>
+                </div>
 
-            <div class ='review-element'>
-                <div class = 'review-label'></div>
-                <div class = 'review-value'> {review.description}</div>
-            </div>
+                {/* todo: adding photos to reviews */}
+                {/* <div className="image-container">
+                    <img src="https://phantom-marca.unidadeditorial.es/9539821a6a594eca33bde1994c03ecd6/resize/1320/f/jpg/assets/multimedia/imagenes/2021/09/05/16308252543285.jpg"className="image"height='200'></img>
+                </div> */}
 
-            <div class ='review-element'>
-                <div class = 'review-label'>Product Id</div>
-                <div class = 'review-value'> {review.productId}</div>
+                <div className='review-element'>
+                    <div className='review-title'></div>
+                    <div className='review-value'>{review.title}</div>
+                </div>
 
-            </div>
+                <div className='review-element'>
+                    <div className='review-label'></div>
+                    <div className='review-value'>{review.description}</div>
+                </div>
+
+                <div className='review-element'>
+                    <div className='review-label'>Product Id</div>
+                    <div className='review-value'>{review.productId}</div>
+                </div>
             </div>
         </div>
-    
-         )
-     }
+    )
+}
         
 
-        export default Review;
+export default Review;
