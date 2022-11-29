6baa4453c7667b6c8c84c5fcb7fbf5d6ebecc35f
Andrew Cook <andrewjcook93@gmail.com>
2022-11-29 11:49:39 -0600

    fixed index error

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
             {
                 path: "profile",
                 element: <Profile />
-            }
+            },
             // SHOP
             {
                 path: "shop",
